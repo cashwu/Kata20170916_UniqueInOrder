bdd38237c520e04a7cd6cc14817f9378aff5f8dc
cash <cash@cashwu.com>
2017-09-16 21:45:44 +0800

    input AB test case

--- Kata20170916_UniqueInOrder/UniqueInOrderTests.cs
+++ Kata20170916_UniqueInOrder/UniqueInOrderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170916_UniqueInOrder
@@ -7,8 +8,24 @@ namespace Kata20170916_UniqueInOrder
     public class UniqueInOrderTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_AB_should_return_AB()
         {
+            UniqueInOrderShouldBe("AB", "AB");
+        }
+
+        private static void UniqueInOrderShouldBe(string expected, string iterable)
+        {
+            var kata = new Kata();
+            var actual = kata.UniqueInOrder(iterable);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            return iterable;
         }
     }
 }
